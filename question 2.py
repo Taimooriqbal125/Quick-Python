5448e537837275ee3411fc94e49c070235c9dcc2
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-10-26 08:55:12 -0700

    Problems

---
+++ question 2.py
@@ -0,0 +1,12 @@
+
+import math
+
+
+angle_value = int (input('Enter a angle value='))
+sin_value = math.sin(angle_value )
+cos_value = math.cos(angle_value )
+tan_value = math.tan(angle_value )
+
+print ('sin:' , sin_value )
+print ('cos:' , cos_value )
+print ('tan:' , tan_value )
