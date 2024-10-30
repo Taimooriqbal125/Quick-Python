47acecfcc1328763238fe981daec9ceb45f95627
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-10-30 06:43:23 -0700

    Problems

---
+++ question 4.py
@@ -0,0 +1,12 @@
+import math 
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
+# Calculate the distance using the distance formula
+distance = math.sqrt ((x2 - x1)**2 + (y2 - y1)**2)
+
+# Output the result
+print("The distance between the points is:" , distance)
+
