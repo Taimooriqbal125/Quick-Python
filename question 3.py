02bdd2ba0e5c8823b7a139bd5ba7b3258865965a
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-10-30 06:41:23 -0700

    Problems

---
+++ question 3.py
@@ -0,0 +1,6 @@
+weight_pound  = float(input('Enter the weight of sugar in pound'))
+pounds_to_kg = 0.453592
+weight_kg = weight_pound * pounds_to_kg
+weight_g = weight_kg * 1000
+print("Weight in kilograms:" , weight_kg)
+print("Weight in grams:" , weight_g)
