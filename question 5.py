58ecda533aec7065ec921f7cc49df2070c1f77dd
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-10-31 07:33:18 -0700

    Problem

---
+++ question 5.py
@@ -0,0 +1,10 @@
+import math 
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+height = float(input("Enter height: "))
+
+# Calculate the volume using the volume formula
+volume = (length * width * height)
+
+# Output the result
+print("The volume is:" , volume )
