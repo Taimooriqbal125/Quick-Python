984fce8bf989b39cff5a15ed042b22157eb274be
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-11-04 13:10:31 -0700

    Problem

---
+++ question 9.py
@@ -0,0 +1,8 @@
+# Input the distance in miles from the user
+miles = float(input("Enter the distance in miles: "))
+
+# Conversion factor: 1 mile = 1.60934 kilometers
+kilometers = miles * 1.60934
+
+# Output the result
+print(f"The distance in kilometers is: {kilometers} km")
